7ac24f2150815125520c9ca7c8e7ad15344406b4
Unknown <mosquitojoe85@gmail.com>
2018-06-25 16:22:21 +0800

    增加SASS相關套件，增加index頁面及相關結構

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
@@ -18,15 +20,30 @@ module.exports = {
           { loader: 'pug-loader', options: { doctype: 'pug', pretty: true } }
         ]
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: { name: 'assets/[name].[ext]' }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 2 } },
+          'sass-loader'
+        ]
+      },
     ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(), //開啟MHR
     new webpack.NamedModulesPlugin(), //MHR更新時，會確實的顯示相關的名稱
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('dev') }),
+    new CleanWebpackPlugin(['dist']), //每次build前清空dist
     new HtmlWebpackPlugin({
-      template: './src/layout.pug',
-      filename:'./layout.html'
+      template: './src/pages/index.pug',
+      filename:'./index.html'
     }),
   ],
   devServer: {
@@ -34,7 +51,7 @@ module.exports = {
     hot: true,
     host: '0.0.0.0',
     before(app, server) {
-      server._watch(`./src/**.pug`);
+      server._watch(`./src/pages/*.pug`);
     }
   }
 
